db0df74b05ff8a091fb621f6aff311e57b37f5f2
Dev-Nawaf <nawaf.alsayah97@gmail.com>
2025-05-15 23:03:55 +0300

    fix sections animation

--- utils.js
+++ utils.js
@@ -1,25 +1,27 @@
 export const clickLabelEvent = (item) => {
-    item.addEventListener("keydown", (e) => {
-        if (e.key === "Enter") {
-            item.click();
-        };
-    })
+  item.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      item.click();
+    }
+  });
 };
 
 export const observeElements = (sectionElements) => {
-    const options = { threshold: 0.5 }//when half of el-class visible
+  const options = { threshold: [0.4, 0.5] }; //when half of el-class visible
 
-    const callback = (entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("in-view")
-            } else {
-                entry.target.classList.remove("in-view")
-            }
-        });
-    }
+  const callback = (entries) => {
+    entries.forEach((entry) => {
+      const ratio = entry.intersectionRatio;
+      if (ratio >= 0.5) {
+        // Add class when threshold is true
+        entry.target.classList.add("in-view");
+      } else if (ratio <= 0.4) {
+        entry.target.classList.remove("in-view");
+      }
+    });
+  };
 
-    const observer = new IntersectionObserver(callback, options);
+  const observer = new IntersectionObserver(callback, options);
 
-    sectionElements.forEach(element => observer.observe(element));
-}
+  sectionElements.forEach((element) => observer.observe(element));
+};
